483b9dd65d112e6b86dfc0f9afc391b2234bf2a4
nanlei <36439285+nanlei2000@users.noreply.github.com>
2019-01-16 16:51:00 +0800

    update

--- index.ts
+++ index.ts
@@ -44,27 +44,3 @@ export const hReduce = (
   })
   return accumulator
 }
-
-const hash = {
-  a: 1,
-  b: 2,
-  c: 3,
-}
-// hEach(hash, (item, key, hash) => {
-//   console.log('​item,key,hash', item, key, hash)
-// })
-
-// let result = hMap(hash, (item, index) => {
-//   return item + index
-// })
-// let result = hFilter(hash, (item, index) => {
-//   return index == 'b'
-// })
-// let result = hReduce(
-//   hash,
-//   (accumulator, item, key) => {
-//     return accumulator + item
-//   },
-//   0
-// )
-// console.log('​result', result)
